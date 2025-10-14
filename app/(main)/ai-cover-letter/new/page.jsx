527ad6b82bd322f9dfcfdc5a0abd07b9bb8a4e1f
Urooj Ashfaq <uroojashfaq8@gmail.com>
2025-10-15 01:15:01 +0500

    Update page.jsx

--- app/(main)/ai-cover-letter/new/page.jsx
+++ app/(main)/ai-cover-letter/new/page.jsx
@@ -6,7 +6,7 @@ import { getCoverLetters } from "@/actions/cover-letter";
 import Link from "next/link";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import CoverLetterList from "./_components/cover-letter-list";
+import CoverLetterList from "../_components/cover-letter-list";
 
 export default async function CoverLetterPage() {
   const coverLetters = await getCoverLetters();
